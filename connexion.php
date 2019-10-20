e66d7cae933fcb31923afe4d59bf877d3559e9a3
yjolivier <bouxouyao@gmail.com>
2019-10-20 00:46:02 +0000

    issertion of the validation condition of:

--- connexion.php
+++ connexion.php
@@ -1,16 +1,66 @@
+<?php
+// On démarre la session AVANT d'écrire du code HTML
+session_start();
+
+//Connexion a la base de donnee
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=olibank;charset=utf8', 'root', '');
+}
+catch (Exception $e) {
+	die('Erreur : ' . $e->getMessage());
+}
+
+if (isset($_POST['connexion'])) {
+	$mailconnect = htmlspecialchars($_POST['mailconnect']);
+	$mdpconnect = password_hash($_POST['mdpconnect'], PASSWORD_DEFAULT);
+
+	if (!empty($_POST['mailconnect']) AND !empty($_POST['mdpconnect'])) {
+		if (filter_var($mailconnect, FILTER_VALIDATE_EMAIL)) {
+			$requser = $bdd->prepare("SELECT * FROM membres WHERE email = ? AND mdpass = ?");
+			$requser->execute(array($mailconnect, $mdpconnect));
+			$userexist = $requser->rowCount();
+			if ($userexist == 1) {
+				$userinfo = $requser->fetch();
+				$_SESSION['id'] = $userinfo['id'];
+				$_SESSION['email'] = $userinfo['email'];
+				$_SESSION['nom'] = $userinfo['nom'];
+				$_SESSION['prenoms'] = $userinfo['prenoms'];
+				header("location: profile.php?id=".$_SESSION['id']);
+			}
+			else {
+				$erreur = 'Adresse mail ou mot de passe incorrect';
+			}
+		}
+		else {
+			$erreur = 'L\'adresse mail est invalide';
+		}
+	}
+	else{
+		$erreur = 'Tout les champs doivent être remplient';
+	}
+}
+?>
+
 <?php require 'header.php'; ?>
 	<body >
 		<div class="container-fluid page-body ">
 			<div class="card-container">
 				<div class="row card-container-small">
 					<div class="card-left col-lg-6 col-md-6 col-sm-12 ">
-						<form id="formulaire" method="POST" action="secret.php">
+						<form id="formulaire" method="POST" action="">
 						<div class="container-form">
 							<h1>Connectez vous ici</h1>
 							<input class="champdesaisir" type="text" name="mailconnect" placeholder="Adresse mail"> <br>
-							<input class="champdesaisir" type="password" name="motdepasse" placeholder="Password"> <br>
-							<input class="form-bouton" name="btnconnect" type="submit" value="Se connecter" />
-							<p>Je veux creer <a href="inscription.php">un compte</a></p>
+							<input class="champdesaisir" type="password" name="mdpconnect" placeholder="Password"> <br>
+							<input class="form-bouton" name="connexion" type="submit" value="Se connecter" />
+							<p>
+								Je veux creer <a href="inscription.php">un compte</a><br>
+								<?php
+									if (isset($erreur)) {
+										echo '<br><font color="red">' . $erreur . '</font>';
+									}
+								?>
+							</p>
 						</div>
 				</form>
 					</div>
--- inscription.php
+++ inscription.php
@@ -1,21 +1,93 @@
-<?php require 'header.php'; ?>
+<?php 
+session_start();
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=olibank;charset=utf8', 'root', '');
+	}
+	catch (Exception $e) {
+	  die('Erreur : ' . $e->getMessage());
+	} 
+	//Verification des donnees envoyees
+	if (isset($_POST['inscription'])) {
+
+		//protection des donnees avec la fonction htmlspecialchars et password_hash
+		$nom = htmlspecialchars($_POST['nom']);
+		$prenoms = htmlspecialchars($_POST['prenoms']);
+		$email = htmlspecialchars($_POST['email']);
+		$email2 = htmlspecialchars($_POST['email2']);
+		/*$mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);*/
+		if (
+			!empty($_POST['nom']) AND 
+			!empty($_POST['prenoms']) AND 
+			!empty($_POST['email']) AND 
+			!empty($_POST['email2']) AND 
+			!empty($_POST['mdp']) AND 
+			!empty($_POST['mdp2'])) 
+		{
+			if ($email === $email2) {
+				if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				  //Verifions si le mail existe deja ou pas.
+					$reqmail = $bdd->prepare("SELECT * FROM membres WHERE mail =?");
+					$reqmail->execute(array($email));
+					$emailexist = $reqmail->rowCount();
+					if ($emailexist == 0) {
+						//On compare les deux mots de passe
+						if ($_POST['mdp'] === $_POST['mdp2']) {
+							$mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
+							$insertmbr = $bdd->prepare("INSERT INTO membres(nom, prenoms, mail, mdpass, date_inscription) VALUES(?, ?, ?, ?, NOW())");
+							$insertmbr->execute(array($nom, $prenoms, $email, $mdp));
+						}
+						else {
+							$erreur = 'Les deux mot de passe sont differents';
+						}
+					}
+					else {
+						$erreur = 'L\'adresse mail existe deja';
+					}
+				}
+				else {
+					$erreur = 'L\'adresse mail n\'est pas valide';
+				}
+			}
+			else {
+				$erreur = "Les adresses mail doivent etre identique !";
+			}
+		}
+		else {
+			$erreur = "Veuillez renseigner toutes les informations !";
+		}
+	}
+?>
+<?php 	require 'header.php'; ?>
 	<body >
 		<div class="container-fluid page-body ">
 			<div class="card-container">
 				<div class="row card-container-small">
 					<div class="card-left col-lg-6 col-md-6 col-sm-12 ">
-						<form id="formulaire" method="POST" action="secret.php">
-						<div class="container-form inscription-form">
-							<h1>Inscrivez vous ici</h1>
-              <input class="champdesaisir" type="text" name="nom" placeholder="Nom"> <br>
-              <input class="champdesaisir" type="text" name="prenoms" placeholder="Prenoms"> <br>
-              <input class="champdesaisir" type="text" name="pseudo" placeholder="Pseudonyme"> <br>
-              <input class="champdesaisir" type="password" name="motdepasse" placeholder="Mot de pass"> <br>
-              <input class="champdesaisir" type="password" name="motdepasse" placeholder="Confirmer le mot de pass"> <br>
-							<input class="form-bouton" name="btn" type="submit" value="Se connecter" />
-							<p>Je veux me <a href="connexion.php">connecter</a> j'ai deja un compte</p>
-						</div>
-				</form>
+						<form id="formulaire" method="POST" action="">
+							<div class="container-form inscription-form">
+								<h1>Inscrivez vous ici</h1>
+								<input class="champdesaisir" type="text" name="nom" placeholder="Nom" value="<?php if(isset($nom)) { echo $nom; } ?>"> 
+								<br>
+								<input class="champdesaisir" type="text" name="prenoms" placeholder="Prenoms" value="<?php if(isset($prenoms)) { echo $prenoms; } ?>"> 
+								<br>
+								<input class="champdesaisir" type="email" name="email" placeholder="Adresse mail" value="<?php if(isset($email)) { echo $email; } ?>"> 
+								<br>
+								<input class="champdesaisir" type="email" name="email2" placeholder="Confirmer votre adresse mail" value="<?php if(isset($email2)) { echo $email2; } ?>"> 
+								<br>
+								<input class="champdesaisir" type="password" name="mdp" placeholder="Mot de pass"> 
+								<br>
+								<input class="champdesaisir" type="password" name="mdp2" placeholder="Confirmer le mot de pass"> 
+								<br>
+								<input class="form-bouton" name="inscription" type="submit" value="Se connecter" />
+								<p>Je veux me <a href="connexion.php">connecter</a> j'ai deja un compte <br>
+								<?php
+									if (isset($erreur)) {
+										echo '<font color="#e32b17">' . $erreur . '</font>'; 
+									}
+								?>
+								</p>
+							</div>
+						</form>
 					</div>
 					<div class="card-right col-lg-6 col-md-6 col-sm-12 ">
 					</div>
