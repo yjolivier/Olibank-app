29c8dd64639dca22a34210975dd8eefed4b8ac4d
yjolivier <bouxouyao@gmail.com>
2019-10-30 00:00:54 +0000

    Customizing the header of the profile.php page

---
+++ profile.php
@@ -0,0 +1,38 @@
+<?php require 'header.php'; ?>
+	<body >
+		<header class="row sticky-top">
+				<div class="header-logo col-lg-4 col-sm-6 col-6">
+					<img src="img/Nsia-logo.png">
+				</div>
+				<div class="header-menu col-lg-8 col-sm-6 col-6">
+					<nav class="navbar navbar-expand-md navbar-white bg-transparent">
+						<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+							<span class="navbar-toggler-icon btn-menu">
+								<i class="fas fa-align-justify"></i>
+							</span>
+						</button>
+						<div class="collapse  navbar-collapse" id="collapse_target">
+							<ul class="navbar-nav">
+								<li class="nav-item">
+									<a class="nav-link" href="#">ACCUEIL</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">A PROPOS</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">CONTACTE</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Deconnexion</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
+			<section>
+				
+			</section>
+	</body>
+	<?php require 'footer.php'; ?>
+</html>
