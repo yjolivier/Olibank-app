448f857c7bc2d3e2f857349934568f0d294998e1
yjolivier <bouxouyao@gmail.com>
2020-04-11 03:47:37 +0000

    Update profile page with new design

--- view/frontend/profile-view.php
+++ view/frontend/profile-view.php
@@ -31,88 +31,92 @@
 						</nav>
 					</div>
 			</header>
-			<div class="slider row"></div>
-			<section class="page-content row">
 			<?php if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id']):?>
-				<div class="content-card-left col-lg-6">
-					<div class="row">
-						<div class="card-img col-12">
-							<img src="public/img/slide.jpg" alt="..." >
-						</div>
-						<div class="title col-12">
-							<h2><?php echo $userinfo['nom'] . ' ' . $userinfo['prenoms']?></h2>
-							<div class="solde-content">
-								<h2>
-								<?php
-									if (isset($CredMont) AND isset($DebMont)){
-										if ($CredMont < $DebMont) {
-											$solde = $DebMont - $CredMont;
-											$_SESSION['mbrsolde'] = $solde;
-											echo 'Solde crediteure : '.$solde;
+			<div class="slider row d-flex justify-content-center"> 
+				<h1><?php echo $userinfo['nom'] . ' ' . $userinfo['prenoms']?></h1>
+			</div>
+			<section class="profile-page-container row d-flex justify-content-center">
+				<div class="col-lg-11 profile-page-content">
+					<div class="content-card-left col-lg-4">
+						<div class="row">
+							<div class="title col-12">
+									<h2>
+									<?php
+										if (isset($CredMont) AND isset($DebMont)){
+											if ($CredMont < $DebMont) {
+												$solde = $DebMont - $CredMont;
+												$_SESSION['mbrsolde'] = $solde;
+												echo 'Solde crediteure ';
+											}
+											else {
+												$solde = $CredMont - $DebMont;
+												$_SESSION['mbrsolde'] = $solde;
+												echo 'Solde Debiteure : ';
+											}
 										}
-										else {
-											$solde = $CredMont - $DebMont;
-											$_SESSION['mbrsolde'] = $solde;
-											echo 'Solde Debiteure : '.$solde.' F';
-										}
-									}
-								?>
+									?>
 								</h2>
 							</div>
+							<div class="card-img col-12 d-flex justify-content-center">
+								<h2 class="align-self-center">
+									<?= $_SESSION['mbrsolde'].' F' ?>
+								</h2>
+							</div>
+							
 						</div>
 					</div>
-				</div>
-				<div class="content-card-right col-lg-6">
-					<div class="row">
-						<div class="historique-title col-12">
-							<h1>HISTORIQUE</h1>
-						</div>
-						<div class="historique-card col-12">
-							<div class="row">
-								<table id="debit-credit" class="col-6" border="1">
-									<thead>
-										<tr align="center">
-											<th colspan="2">debit</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<th>date</th>
-											<th>montant</th>
-										</tr>
-										<?php if(!empty($debit)){ foreach ($debit as $k => $value) {?>
-										<tr>
-											<td><?= $value['date'] ?></td>
-											<td><?= $value['montant']?></td>
-										</tr>
-										<?php }} ?>
-									</tbody>
-								</table>
-								<table id="debit-credit" class="col-6" border="1">
-									<thead>
-										<tr align="center">
-											<th colspan="2">credit</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<th>date</th>
-											<th>montant</th>
-										</tr>
-										<?php if(!empty($credit)){ foreach ($credit as $k => $value) {?>
-										<tr>
-											<td><?= $value['date'] ?></td>
-											<td><?= $value['montant']?></td>
-										</tr>
-										<?php } }?>
-									</tbody>
-								</table>
+					<div class="content-card-right col-lg-8">
+						<div class="row">
+							<!--<div class="historique-title col-12">
+								<h1>HISTORIQUE</h1>
+							</div>-->
+							<div class="historique-card col-12">
+								<div class="row">
+									<table id="debit-credit" class="col-md-6 col-12" border="1">
+										<thead>
+											<tr align="center">
+												<th colspan="2">debit</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<th>date</th>
+												<th>montant</th>
+											</tr>
+											<?php if(!empty($debit)){ foreach ($debit as $k => $value) {?>
+											<tr>
+												<td><?= $value['date'] ?></td>
+												<td><?= $value['montant']?></td>
+											</tr>
+											<?php }} ?>
+										</tbody>
+									</table>
+									<table id="debit-credit" class="col-md-6 col-12" border="1">
+										<thead>
+											<tr align="center">
+												<th colspan="2">credit</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<th>date</th>
+												<th>montant</th>
+											</tr>
+											<?php if(!empty($credit)){ foreach ($credit as $k => $value) {?>
+											<tr>
+												<td><?= $value['date'] ?></td>
+												<td><?= $value['montant']?></td>
+											</tr>
+											<?php } }?>
+										</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<?php endif; ?>
 			</section>
+			<?php endif; ?>
 			<footer class="row">
 					<div class="col-12">
 						<p>©Designed by OtoiSofware</p>
